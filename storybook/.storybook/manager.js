236f6ec11a5db5c1d9cbbade6996db96f18526e1
Adrien Poupard <1469248+apoupard@users.noreply.github.com>
2024-03-19 00:18:29 +0100

    Update docs cicd

---
+++ storybook/.storybook/manager.js
@@ -0,0 +1,13 @@
+import { addons } from '@storybook/addons';
+import {create} from "@storybook/theming";
+import logo from "../assets/logo.png";
+
+addons.setConfig({
+    theme: create({
+        base: 'light',
+        brandTitle: 'Connect FS',
+        brandUrl: "https://komune-io.github.io/connect-fs/",
+        brandImage: logo,
+    }),
+    showToolbar: false
+});
